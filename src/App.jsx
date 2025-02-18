8288637c9618446635fb525641efb65ae88abb57
Fer Fuentes <fernandocastillo.8@hotmail.com>
2025-02-18 09:56:36 -0600

    Creación y configuración inicial del repositorio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Routes } from 'react-router'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+        <Routes>
+
+            {/* Colocar las rutas acá */}
+
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
