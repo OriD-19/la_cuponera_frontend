9c84bb9f29e6fdb7062a49c350dca15d092651d4
Michior <111088211+Michior@users.noreply.github.com>
2025-02-19 14:17:11 -0600

    Vista de cupones sin token

--- src/VisualizacionCupones/CuponCard.jsx
+++ src/VisualizacionCupones/CuponCard.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React from "react";
 
-export const CuponCard = () => {
+export const CuponCard = ({ cupon }) => {
     return (
-        <div className="relative bg-fondo p-4 rounded-lg shadow-md w-full h-auto mx-auto ">
+        <div className="relative bg-fondo p-4 rounded-lg shadow-md w-full h-auto mx-auto">
             <div className="absolute top-2 right-2 bg-resaltador text-white text-sm font-bold px-2 py-1 rounded-full">
                 <p>%</p>
             </div>
-            <div className='h-auto'>
-                <img src="./public/img/Cuponazo.png" alt="imagen ilustrativa" className='place-self-center w-auto h-40'/>
-                <p className='mt-3 font-bold'>Descripción de la promoción</p>
-                <p className='font-semibold'>Empresa</p>
-                <p>Fecha</p>
+            <div className="h-auto">
+                <img src="./public/img/Cuponazo.png" alt="imagen ilustrativa" className="place-self-center w-auto h-40" />
+                <p className="mt-3 font-bold">{cupon.title}</p>
+                <p className="font-semibold">{cupon.enterpriseCode}</p>
+                <p>Desde: {new Date(cupon.validFrom).toLocaleDateString()} - Hasta: {new Date(cupon.validUntil).toLocaleDateString()}</p>
+                <p>Precio Regular: ${cupon.regularPrice}</p>
+                <p>Precio Oferta: ${cupon.offerPrice}</p>
+                <p>{cupon.offerDesc}</p>
             </div>
-            <button className="bg-primary h-7 w-full mt-4 rounded text-white items-center text-center hover:bg-resaltador transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-96">Ver oferta</button>
+            <button className="bg-primary h-7 w-full mt-4 rounded text-white items-center text-center hover:bg-resaltador transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-96">
+                Ver oferta
+            </button>
         </div>
-    )
-}
+    );
+};
