2fb602e85eeb5107b7af47af08827ccfad97429f
FernandaRubio74 <20235795@esen.edu.sv>
2025-02-19 01:40:56 -0600

    base

--- src/VisualizacionCupones/CuponCard.jsx
+++ src/VisualizacionCupones/CuponCard.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 
 export const CuponCard = () => {
     return (
-        <div className="relative bg-blue-300 p-4 rounded-lg shadow-md ">
-            <div className="absolute top-2 right-2 bg-red-600 text-white text-sm font-bold px-2 py-1 rounded-full">
+        <div className="relative bg-fondo p-4 rounded-lg shadow-md w-full h-auto mx-auto ">
+            <div className="absolute top-2 right-2 bg-resaltador text-white text-sm font-bold px-2 py-1 rounded-full">
                 <p>%</p>
             </div>
-            <div className="bg-blue-900 h-6 w-3/4 mt-4 rounded">ACA LA INFO DEL CUPON</div>
+            <div className='h-auto'>
+                <img src="./public/img/Cuponazo.png" alt="imagen ilustrativa" className='place-self-center w-auto h-40'/>
+                <p className='mt-3 font-bold'>Descripción de la promoción</p>
+                <p className='font-semibold'>Empresa</p>
+                <p>Fecha</p>
+            </div>
+            <button className="bg-primary h-7 w-full mt-4 rounded text-white items-center text-center hover:bg-resaltador transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-96">Ver oferta</button>
         </div>
     )
 }
---
+++ src/VisualizacionCupones/filtro/DropDownMenu.jsx
@@ -0,0 +1,59 @@
+// Código gracias a documentacion de 'Tailwindcss':https://tailwindui.com/components/application-ui/elements/dropdowns y https://headlessui.com/react/menu
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
+export default function Categorias() {
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-mdborder-3 bg-base border-3 border-resaltador px-6 py-2 rounded text-resaltador font-semibold shadow-xs hover:bg-gray-50">
+          Categoría
+          <ChevronDownIcon className="size-5.5 fill-resaltador" />
+        </MenuButton>
+      </div>
+
+      <MenuItems
+        transition
+        className="absolute left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+      >
+        <div className="py-1">
+          <MenuItem>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-black data-focus:bg-gray-100 data-focus:text-resaltador"
+            >
+              Restaurantes
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-black data-focus:bg-gray-100 data-focus:text-resaltador"
+            >
+              Belleza
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-black data-focus:bg-gray-100 data-focus:text-resaltador"
+            >
+              Hogar
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-black data-focus:bg-gray-100 data-focus:text-resaltador"
+            >
+              Salud
+            </a>
+          </MenuItem>
+        </div>
+      </MenuItems>
+    </Menu>
+  )
+}
+
+
+
